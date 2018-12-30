2db7708c5ae667755820859009d8f5df451bac3b
James Thompson <james@thomps.onl>
2018-12-29 20:09:19 -0700

    Adds git aliases and config to setup script.

--- setup.sh
+++ setup.sh
@@ -6,3 +6,15 @@
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -nfs $SOURCE_DIR/bash/profile $HOME/.bash_profile
+
+# Configure global git settings
+git config --global user.name "James Thompson"
+git config --global user.email "james@thomps.onl"
+git config --global core.excludesfile $SOURCE_DIR/.gitignore
+
+# Configure global git aliases
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.unstage 'reset HEAD --'
