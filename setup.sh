3ee699bae2e2d4212fcca48497826b03ebd51d1e
James Thompson <james@thomps.onl>
2018-12-29 19:57:31 -0700

    Adopts MIT license.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# Copyright (c) 2018 James Thompson <james@thomps.onl>
+# This code is licensed under the MIT license (see LICENSE for details)
+
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -nfs $SOURCE_DIR/bash/profile $HOME/.bash_profile
