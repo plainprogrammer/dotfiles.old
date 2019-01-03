885c99ec92350f3c1871a27fb295b6ef6d74710e
James Thompson <james@thomps.onl>
2019-01-03 15:48:58 -0700

    Adds GNU utilities, Visual Studio Code and JetBrain Toolbox.

--- setup.sh
+++ setup.sh
@@ -7,6 +7,62 @@ SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cat $SOURCE_DIR/README
 
+if [[ -z $(command -v brew) ]]
+  then
+    echo 'Starting installation of Homebrew...'
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+if [[ -n $(command -v brew) ]]
+  then
+    echo 'Installing GNU tools...'
+    brew install coreutils
+    brew install diffutils
+    brew install ed --with-default-names
+    brew install findutils --with-default-names
+    brew install gawk
+    brew install gnu-indent --with-default-names
+    brew install gnu-sed --with-default-names
+    brew install gnu-tar --with-default-names
+    brew install gnu-which --with-default-names
+    brew install gnutls
+    brew install grep --with-default-names
+    brew install gzip
+    brew install screen
+    brew install watch
+    brew install wdiff --with-gettext
+    brew install wget
+    brew install bash
+    brew install gpatch
+    brew install less
+    brew install m4
+    brew install make --with-default-names
+    brew install nano
+    brew install git
+    brew install openssh
+    brew install rsync
+    brew install unzip
+    brew install vim --override-system-vi
+
+    if [[ -z $(brew cask list | grep visual-studio-code) ]]
+      then
+        echo -e "Installing Visual Studio Code"
+        brew cask install visual-studio-code
+    fi
+
+    if [[ -z $(brew cask list | grep jetbrains-toolbox) ]]
+      then
+        echo -e "Installing JetBrains Toolbox"
+        brew cask install jetbrains-toolbox
+    fi
+
+    echo -e "Installing Homebrew fonts"
+    brew tap homebrew/cask-fonts
+    brew cask install font-fira-code
+    brew cask install font-fira-mono
+    brew cask install font-fira-sans
+fi
+
 echo 'Symlinking Bash profile into place...'
 ln -nfs $SOURCE_DIR/bash/profile $HOME/.bash_profile
 
@@ -24,14 +80,5 @@ git config --global alias.unstage 'reset HEAD --'
 
 echo 'Reloading shell settings...'
 source $HOME/.bash_profile
-
-echo -e "Installing Homebrew fonts."
-if [[ -n $(command -v brew) ]]
-  then
-    brew tap homebrew/cask-fonts
-    brew cask install font-fira-code
-    brew cask install font-fira-mono
-    brew cask install font-fira-sans
-fi
 
 echo -e "Done \xE2\x9C\x94"
